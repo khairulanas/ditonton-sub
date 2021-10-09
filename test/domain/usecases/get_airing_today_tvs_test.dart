6c0a0c2dec47b719f58896494a294cf86ed8dbc9
khairul anas <mafiakanan@gmail.com>
2021-10-09 13:17:01 +0700

    domain usecase  & test

---
+++ test/domain/usecases/get_airing_today_tvs_test.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/domain/entities/tv.dart';
+import 'package:ditonton/domain/usecases/get_airing_today_tvs.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late GetAiringTodayTvs usecase;
+  late MockTvRepository mockTvRepository;
+
+  setUp(() {
+    mockTvRepository = MockTvRepository();
+    usecase = GetAiringTodayTvs(mockTvRepository);
+  });
+
+  final tTvs = <Tv>[];
+
+  group('GetAiringTodayTvs Test: ', () {
+    test('should get list of tv series from the repository', () async {
+      // arrange
+      when(mockTvRepository.getAiringTodayTvs())
+          .thenAnswer((_) async => Right(tTvs));
+      // act
+      final result = await usecase.execute();
+      // assert
+      expect(result, Right(tTvs));
+    });
+  });
+}
--- test/dummy_data/dummy_objects.dart
+++ test/dummy_data/dummy_objects.dart
@@ -2,6 +2,13 @@ import 'package:ditonton/data/models/movie_table.dart';
 import 'package:ditonton/domain/entities/genre.dart';
 import 'package:ditonton/domain/entities/movie.dart';
 import 'package:ditonton/domain/entities/movie_detail.dart';
+import 'package:ditonton/domain/entities/tv.dart';
+import 'package:ditonton/domain/entities/tv_detail.dart';
+import 'package:ditonton/domain/entities/tv_last_episode_to_air.dart';
+import 'package:ditonton/domain/entities/tv_network.dart';
+import 'package:ditonton/domain/entities/tv_production_country.dart';
+import 'package:ditonton/domain/entities/tv_season.dart';
+import 'package:ditonton/domain/entities/tv_spoken_languange.dart';
 
 final testMovie = Movie(
   adult: false,
@@ -20,7 +27,24 @@ final testMovie = Movie(
   voteCount: 13507,
 );
 
+final testTv = Tv(
+  backdropPath: 'backdropPath',
+  firstAirDate: DateTime.now(),
+  genreIds: [10, 20],
+  id: 1,
+  name: 'name',
+  originCountry: ['a', 'b'],
+  originalLanguage: 'originalLanguage',
+  originalName: 'originalName',
+  overview: 'overview',
+  popularity: 5.0,
+  posterPath: 'posterPath',
+  voteAverage: 5.0,
+  voteCount: 1,
+);
+
 final testMovieList = [testMovie];
+final testTvList = [testTv];
 
 final testMovieDetail = MovieDetail(
   adult: false,
@@ -35,6 +59,72 @@ final testMovieDetail = MovieDetail(
   title: 'title',
   voteAverage: 1,
   voteCount: 1,
+);
+
+final testTvDetail = TvDetail(
+  backdropPath: 'backdropPath',
+  createdBy: ['a', 'b'],
+  episodeRunTime: [1, 2],
+  firstAirDate: DateTime.now(),
+  genres: [Genre(id: 1, name: 'Action')],
+  homepage: 'homepage',
+  id: 1,
+  inProduction: true,
+  languages: ['aa', 'bb'],
+  lastAirDate: DateTime.now(),
+  lastEpisodeToAir: TvLastEpisodeToAir(
+      airDate: DateTime.now(),
+      episodeNumber: 1,
+      id: 1,
+      name: 'name',
+      overview: 'overview',
+      productionCode: 'productionCode',
+      seasonNumber: 1,
+      stillPath: 'stillPath',
+      voteAverage: 0,
+      voteCount: 1),
+  name: 'name',
+  nextEpisodeToAir: 12,
+  networks: [tvNetwork],
+  numberOfEpisodes: 1,
+  numberOfSeasons: 1,
+  originCountry: ['aa'],
+  originalLanguage: 'originalLanguage',
+  originalName: 'originalName',
+  overview: 'overview',
+  popularity: 1.1,
+  posterPath: 'posterPath',
+  productionCompanies: [tvNetwork],
+  productionCountries: [
+    TvProductionCountry(iso31661: 'iso31661', name: 'name')
+  ],
+  seasons: [
+    TvSeason(
+      airDate: DateTime.now(),
+      episodeCount: 1,
+      id: 1,
+      name: 'name',
+      overview: 'overview',
+      posterPath: 'posterPath',
+      seasonNumber: 1,
+    ),
+  ],
+  spokenLanguages: [
+    TvSpokenLanguage(
+        englishName: 'englishName', iso6391: 'iso6391', name: 'name')
+  ],
+  status: 'status',
+  tagline: 'tagline',
+  type: 'type',
+  voteAverage: 1.1,
+  voteCount: 1,
+);
+
+final tvNetwork = TvNetwork(
+  name: 'name',
+  id: 1,
+  logoPath: 'logoPath',
+  originCountry: 'originCountry',
 );
 
 final testWatchlistMovie = Movie.watchlist(
