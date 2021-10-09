48f7cb57464443a9e31e29cef50f3829f5e12124
khairul anas <mafiakanan@gmail.com>
2021-10-09 08:04:33 +0700

    tv response model

--- lib/data/models/tv_model.dart
+++ lib/data/models/tv_model.dart
@@ -28,7 +28,7 @@ class TvModel extends Equatable {
   final String originalName;
   final String overview;
   final double popularity;
-  final String posterPath;
+  final String? posterPath;
   final double voteAverage;
   final int voteCount;
 
