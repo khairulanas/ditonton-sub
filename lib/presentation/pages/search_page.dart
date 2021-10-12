a083b954dd93625e5705715ea76e5a56e87367fb
khairul anas <mafiakanan@gmail.com>
2021-10-12 18:32:11 +0700

    search page

--- lib/presentation/pages/search_page.dart
+++ lib/presentation/pages/search_page.dart
@@ -1,69 +1,138 @@
 import 'package:ditonton/common/constants.dart';
 import 'package:ditonton/common/state_enum.dart';
 import 'package:ditonton/presentation/provider/movie_search_notifier.dart';
+import 'package:ditonton/presentation/provider/tv_search_notifier.dart';
 import 'package:ditonton/presentation/widgets/movie_card_list.dart';
+import 'package:ditonton/presentation/widgets/tv_card_list.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class SearchPage extends StatelessWidget {
   static const ROUTE_NAME = '/search';
+  final List<String> _title = ['Movies', 'Tv Show'];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Search'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              onSubmitted: (query) {
-                Provider.of<MovieSearchNotifier>(context, listen: false)
-                    .fetchMovieSearch(query);
-              },
-              decoration: InputDecoration(
-                hintText: 'Search title',
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Search'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                onSubmitted: (query) {
+                  Provider.of<MovieSearchNotifier>(context, listen: false)
+                      .fetchMovieSearch(query);
+                  Provider.of<TvSearchNotifier>(context, listen: false)
+                      .fetchTvSearch(query);
+                },
+                decoration: InputDecoration(
+                  hintText: 'Search title',
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(),
+                ),
+                textInputAction: TextInputAction.search,
               ),
-              textInputAction: TextInputAction.search,
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Search Result',
-              style: kHeading6,
-            ),
-            Consumer<MovieSearchNotifier>(
-              builder: (context, data, child) {
-                if (data.state == RequestState.Loading) {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else if (data.state == RequestState.Loaded) {
-                  final result = data.searchResult;
-                  return Expanded(
-                    child: ListView.builder(
-                      padding: const EdgeInsets.all(8),
-                      itemBuilder: (context, index) {
-                        final movie = data.searchResult[index];
-                        return MovieCard(movie);
-                      },
-                      itemCount: result.length,
-                    ),
-                  );
-                } else {
-                  return Expanded(
-                    child: Container(),
-                  );
-                }
-              },
-            ),
-          ],
+              SizedBox(height: 16),
+              Text(
+                'Search Result',
+                style: kHeading6,
+              ),
+              TabBar(tabs: [
+                Tab(
+                  text: _title[0],
+                ),
+                Tab(
+                  text: _title[1],
+                ),
+              ]),
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    _MovieSearchResult(),
+                    _TvSearchResult(),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class _MovieSearchResult extends StatelessWidget {
+  const _MovieSearchResult({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<MovieSearchNotifier>(
+      builder: (context, data, child) {
+        if (data.state == RequestState.Loading) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (data.state == RequestState.Loaded) {
+          final result = data.searchResult;
+          return Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemBuilder: (context, index) {
+                final movie = data.searchResult[index];
+                return MovieCard(movie);
+              },
+              itemCount: result.length,
+            ),
+          );
+        } else {
+          return Expanded(
+            child: Container(),
+          );
+        }
+      },
+    );
+  }
+}
+
+class _TvSearchResult extends StatelessWidget {
+  const _TvSearchResult({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TvSearchNotifier>(
+      builder: (context, data, child) {
+        if (data.state == RequestState.Loading) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (data.state == RequestState.Loaded) {
+          final result = data.searchResult;
+          return Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemBuilder: (context, index) {
+                final tv = data.searchResult[index];
+                return TvCard(tv);
+              },
+              itemCount: result.length,
+            ),
+          );
+        } else {
+          return Expanded(
+            child: Container(),
+          );
+        }
+      },
     );
   }
 }
