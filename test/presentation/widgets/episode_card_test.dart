f57423f2055bb250c5867a0730fdc32fe4ab53b9
khairul anas <mafiakanan@gmail.com>
2021-10-12 12:19:11 +0700

    test widget

---
+++ test/presentation/widgets/episode_card_test.dart
@@ -0,0 +1,29 @@
+import 'package:ditonton/domain/entities/tv_episode.dart';
+import 'package:ditonton/presentation/widgets/episode_card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final episode = TvEpisode(
+      airDate: DateTime.parse("2021-02-12"),
+      episodeNumber: 1,
+      id: 2723622,
+      name: "Episode 5.5: Guidepost",
+      overview:
+          "A special episode detailing the events leading up to the current situation with the Grace Field Farm children.",
+      productionCode: "",
+      seasonNumber: 0,
+      stillPath: "/aHqMdHgzOTGLFYCZxXrUyywq67l.jpg",
+      voteAverage: 0.0,
+      voteCount: 0);
+  group('Episode card Widget Test', () {
+    Widget _makeTestableWidget() {
+      return MaterialApp(home: Scaffold(body: EpisodeCard(episode)));
+    }
+
+    testWidgets('Testing if title shows up', (WidgetTester tester) async {
+      await tester.pumpWidget(_makeTestableWidget());
+      expect(find.byType(Text), findsWidgets);
+    });
+  });
+}
