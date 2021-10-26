9faaec12fba4ada39284fe1b0e88d8ce85f680df
khairul anas <mafiakanan@gmail.com>
2021-10-26 09:48:25 +0700

    update json reader :core module test github ci

--- core/test/json_reader.dart
+++ core/test/json_reader.dart
@@ -1,9 +1,14 @@
 import 'dart:io';
+import 'package:path/path.dart' as p;
 
 String readJson(String name) {
   var dir = Directory.current.path;
-  if (dir.endsWith('/core/test')) {
-    dir = dir.replaceAll('/core/test', '');
+  print(dir);
+  if (dir.endsWith('/test')) {
+    dir = dir.replaceAll(p.join('/test'), '');
   }
-  return File('$dir/core/test/$name').readAsStringSync();
+  if (dir.endsWith('/core') || dir.endsWith('\core')) {
+    return File(p.join(dir, 'test', name)).readAsStringSync();
+  }
+  return File(p.join(dir, 'core', 'test', name)).readAsStringSync();
 }
