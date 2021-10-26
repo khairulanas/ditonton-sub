f3a66a63af28f040741a2b303bd1030ba4067f31
khairul anas <mafiakanan@gmail.com>
2021-10-26 09:09:15 +0700

    update json reader: test codemagic

--- core/test/json_reader.dart
+++ core/test/json_reader.dart
@@ -2,8 +2,8 @@ import 'dart:io';
 
 String readJson(String name) {
   var dir = Directory.current.path;
-  if (dir.endsWith('/test')) {
-    dir = dir.replaceAll('/test', '');
+  if (dir.endsWith('/core/test')) {
+    dir = dir.replaceAll('/core/test', '');
   }
-  return File('$dir/test/$name').readAsStringSync();
+  return File('$dir/core/test/$name').readAsStringSync();
 }
--- tv_series/test/json_reader.dart
+++ tv_series/test/json_reader.dart
@@ -2,8 +2,8 @@ import 'dart:io';
 
 String readJson(String name) {
   var dir = Directory.current.path;
-  if (dir.endsWith('/test')) {
-    dir = dir.replaceAll('/test', '');
+  if (dir.endsWith('/tv_series/test')) {
+    dir = dir.replaceAll('/tv_series/test', '');
   }
-  return File('$dir/test/$name').readAsStringSync();
+  return File('$dir/tv_series/test/$name').readAsStringSync();
 }
