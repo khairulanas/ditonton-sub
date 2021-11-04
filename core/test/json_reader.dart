3d82b4efd9d614494fea0d837faef93b69c7ce03
Ka <50281133+khairulanas@users.noreply.github.com>
2021-11-05 06:30:09 +0700

    tes

--- core/test/json_reader.dart
+++ core/test/json_reader.dart
@@ -1,14 +1,12 @@
 import 'dart:io';
-import 'package:path/path.dart' as p;
 
 String readJson(String name) {
   var dir = Directory.current.path;
-  print(dir);
   if (dir.endsWith('/test')) {
-    dir = dir.replaceAll(p.join('/test'), '');
+    dir = dir.replaceAll('/test', '');
   }
   if (dir.endsWith('/core') || dir.endsWith('\core')) {
-    return File(p.join(dir, 'test', name)).readAsStringSync();
+    return File('$dir/test/$name').readAsStringSync();
   }
-  return File(p.join(dir, 'core', 'test', name)).readAsStringSync();
+  return File('$dir/core/test/$name').readAsStringSync();
 }
